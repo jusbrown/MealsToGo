0192c227de011d5befa15f65aedacf1418bc0c64
Justin Brown <justin@pangotek.com>
2021-11-16 02:58:10 -0800

    Add navigation and finish restaurant screen

---
+++ src/features/restaurants/screens/restaurant.styles.js
@@ -0,0 +1,12 @@
+import { FlatList } from "react-native";
+import styled from "styled-components/native";
+
+export const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
---
+++ src/features/settings/screens/settings.screen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeArea } from "../../../components/utility/safe-area.component";
+
+export const SettingsScreen = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
